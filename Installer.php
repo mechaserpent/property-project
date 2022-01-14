928cb0a1ec846e28c36d4c0f02d2ee4a8c5e9f52
Raymond Chong <mathsgod@yahoo.com>
2022-01-14 16:10:24 +0800

    init

--- Installer.php
+++ Installer.php
@@ -8,4 +8,21 @@ class Installer
     {
         echo "Database setup...\n";
     }
+
+    function input(string $prompt = null): string
+    {
+        echo $prompt;
+        $handle = fopen("php://stdin", "r");
+        $output = fgets($handle);
+        return trim($output);
+    }
+
+    function generateRandomString($length = 128)
+    {
+        $string = '';
+        $string .= '0123456789';
+        $string .= 'abcdefghijklmnopqrstuvwxyz';
+        $string .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return substr(str_shuffle(str_repeat($x = $string, ceil($length / strlen($x)))), 1, $length);
+    }
 }
