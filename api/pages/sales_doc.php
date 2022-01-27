7c5e983ce29e3e110cf21edf99c4d95cdb60ece9
maverick.hlc <36224215+mechaserpent@users.noreply.github.com>
2022-01-27 11:33:56 +0800

    Update sales_doc.php

--- api/pages/sales_doc.php
+++ api/pages/sales_doc.php
@@ -12,38 +12,37 @@ return new class
             "sales brochure",
             "price list",
             "sales arrangements",
-            "tender document",
+            "tender documents",
             "register of transactions",
             "deed of mutual covenant",
             "aerial photograph",
             "sales brochure for parking spaces",
             "price list for parking spaces",
             "ballot result for parking spaces"
-
         ];
+
         $sv_locale = ["en", "tc", "sc"];
-        $query_type = $context->query["type"];  //      $query_type = $this->_query["type"];
-        $query_language = $context->query["locale"];
+        $query_type = strtolower($context->query["type"]);  //      $query_type = $this->_query["type"];
+        $query_language = strtolower($context->query["locale"]);
 
         $res = array_search($query_language, $sv_locale);
-
         if ($res === false) return http_response_code(400);
 
-        $res = array_search(str_replace("+", " ",  strtolower($query_type)), $sv_type);
-        if ($res === false) return http_response_code(400);
+
+        $res = array_search(str_replace("+", " ",  $query_type), $sv_type);
+        if ($res === false) return [http_response_code(400)];
         $w[] = "type = $res";
 
         if (intval($context->query["preview"])) $w[] = "status = 0";
-        else $w[] ="status = 1";
+        else $w[] = "status = 1";
 
         //                                                              DOCUMENTATION    ===>    https://docs.laminas.dev/laminas-db/sql/
 
-        if ($query_language) {
+        if ($res !== false) {
             $data = Updates::Query();
             $data->where($w);
             $data->order(["date" => "DESC", "sequence" => "DESC"]);
-            //print_r($data->getSqlString());            
-            //print_r($data->toArray());
+            //print_r($data->getSqlString());
             $data = $data->map(function (Updates $updates) use ($query_language, $query_type) {
                 $updates->title = $updates->{"title_" . $query_language} ? $updates->{"title_" . $query_language} : $updates->title_en;
                 $updates->file = $updates->{"file_" . $query_language} ? $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . "/uploads/" . $updates->{"file_" . $query_language} : $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . "/uploads/" . $updates->file_en;
