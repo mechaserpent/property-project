f7b5a0901f0184400e2b9347e17dea6efc4f6fa8
maverick.hlc <36224215+mechaserpent@users.noreply.github.com>
2022-01-27 11:34:46 +0800

    Update get_lastupdate.php

--- api/pages/get_lastupdate.php
+++ api/pages/get_lastupdate.php
@@ -11,17 +11,17 @@ return new class
         $sv_locale = ["en", "tc", "sc"];
         $query_language = strtolower($context->query["locale"]);
         $res = array_search($query_language, $sv_locale);
-        
+
         $data["date"] = ($query_language == "en") ? date("j F Y", strtotime(reset($arr))) :  date("Y年n月j日", strtotime(reset($arr)));
-        if ($res === false) return $data; 
+        if ($res === false) return $data;
 
         $arr["updated_time"] = Updates::Query()->where("status = 1")->order(["updated_time" => "DESC"])->first()->updated_time;
         $arr["created_time"] = Updates::Query()->where("status = 1")->order(["created_time" => "DESC"])->first()->created_time;
         arsort($arr);
-        
+
 
         $data["date"] = ($query_language == "en") ? date("j F Y", strtotime(reset($arr))) :  date("Y年n月j日", strtotime(reset($arr)));
 
         return $data ?? [];
-    }  
+    }
 };
