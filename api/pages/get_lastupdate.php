e6ce314b8bc169fbebc93a2d5c05461c9f10180c
maverick.hlc <36224215+mechaserpent@users.noreply.github.com>
2022-04-06 15:17:18 +0800

    Update get_lastupdate.php

--- api/pages/get_lastupdate.php
+++ api/pages/get_lastupdate.php
@@ -6,22 +6,23 @@ return new class
 {
     public function get(Context $context)
     {
-        $arr["lastUpdate"] = Config::Query()->where("name = 'lastUpdate'")->first()->value;
-
+        
         $sv_locale = ["en", "tc", "sc"];
         $query_language = strtolower($context->query["locale"]);
         $res = array_search($query_language, $sv_locale);
-
-        $data["date"] = ($query_language == "en") ? date("j F Y", strtotime(reset($arr))) :  date("Y年n月j日", strtotime(reset($arr)));
-        if ($res === false) return $data;
-
+        
+        if ($res === false) return http_response_code(400);
+        
+        
+        $arr["lastUpdate"] = Config::Query()->where("name = 'lastUpdate'")->first()->value;
         $arr["updated_time"] = Updates::Query()->where("status = 1")->order(["updated_time" => "DESC"])->first()->updated_time;
         $arr["created_time"] = Updates::Query()->where("status = 1")->order(["created_time" => "DESC"])->first()->created_time;
         arsort($arr);
 
+        if(!reset($arr)) return "";
 
         $data["date"] = ($query_language == "en") ? date("j F Y", strtotime(reset($arr))) :  date("Y年n月j日", strtotime(reset($arr)));
 
-        return $data ?? [];
+        return $data["date"];
     }
 };
